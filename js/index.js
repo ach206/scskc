e8b85815ff16941a938f194d484d88e9d9963b58
Camille Hall <camille@creativeambition.net>
2018-07-22 16:29:58 -0700

    updated link path

--- js/index.js
+++ js/index.js
@@ -33,48 +33,54 @@
 		}
 		open();
 
-		// $(".fa-bars").click(function(event){
-		// 	$("#navLinks").attr("class", "showDrawer");
-		// 	$(".menuLinks").attr("class", "showMenuLinks");
-		// 	event.stopPropagation();
-		// });
+// --------------function to loop through the 
+//simages and hide them by default--------
+let slideIndex = 0;
+let i;
+let slides = document.querySelectorAll(".slideImg");
+let dials = document.getElementsByClassName("dial");
+let forward = document.querySelector('.next');
+let back = document.querySelector('.prev');
 
-		// $("body").click(function(){
-		// 	$("#navLinks").removeClass("showDrawer");
-		// 	$(".showMenuLinks").attr("class", "menuLinks");
+function slideshow() {	
+	// hides active styling until it's later added back
+for( i = 0; i < dials.length; i++) {
+	dials[i].className = dials[i].className.replace(" active", "");
+	slides[i].style.display = "none";
+}
+//increases by 1
+slideIndex++;
 
-		// });
+//
+if (slideIndex > slides.length) {
+slideIndex = 1;
+}
 
-		})();
-// --------------function to loop through the images and hide them by default--------
-var slideIndex = 0;
+    slides[slideIndex-1].style.display = "block";
+    dials[slideIndex-1].className += " active";
+
+  forward.addEventListener('click', function(e){
+
+         if (slideIndex === dials.length) {
+          slideIndex = 0;
+          } else {
+          	slideshow();
+        }
+  	
+    });
+   back.addEventListener('click', function(e){
+  	        if (slideIndex === 3) {
+          slideIndex = 0;
+        } else {
+          	slideshow();
+      }
+ 		
+    });
+
+const transition = setTimeout(slideshow, 4000); /*Change image every 4 seconds*/
+}// --------close slideshow function------
 slideshow();
 
-function slideshow() {
-var i;
-var slide = document.getElementsByClassName("slideImg");
-var dial = document.getElementsByClassName("dial");
-for(i = 0; i < slide.length; i++) {
-	// hide slides one by one
-	slide[i].style.display = "none";
-	}
+})();
 
-slideIndex++;
-if (slideIndex > slide.length) {
-slideIndex = 1
-}
 
-for( i = 0; i < dial.length; i++) {
-	dial[i].className = dial[i].className.replace(" active", "");
-	// temporairly disables active styling until loop activates it
-}
-
-    slide[slideIndex-1].style.display = "block";
-    dial[slideIndex-1].className += " active";
-    setTimeout(slideshow, 4000); /*Change image every 4 seconds*/
-}// --------close slideshow function------
-
-// changes the icon when the drawer opens
-// $("#navIcon").on("click", function switchIcon(){
-// 	$("#navIcon").toggleClass("fa-bars fa-times");
-// });
